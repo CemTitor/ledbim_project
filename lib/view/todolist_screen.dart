809637e7afd741805f2d7d1955f9c8d0da4f0181
CemYilmaz <cemyilmaz463@gmail.com>
2023-02-06 02:22:13 +0300

    checkbox added

--- lib/view/todolist_screen.dart
+++ lib/view/todolist_screen.dart
@@ -22,6 +22,14 @@ class _TodoListScreenState extends State<TodoListScreen> {
         itemCount: _todos.length,
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
+            leading: Checkbox(
+              value: _todos[index].isDone,
+              onChanged: (bool? value) {
+                setState(() {
+                  _todos[index].isDone = value!;
+                });
+              },
+            ),
             title: Text('${_todos[index].name}'),
             trailing: IconButton(
               icon: const Icon(Icons.delete),
@@ -46,37 +54,36 @@ class _TodoListScreenState extends State<TodoListScreen> {
 
   void _addTodo() {
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-      String? taskName;
-      return AlertDialog(
-
-        title: const Text('Add Todo'),
-        content: TextField(
-          autofocus: true,
-          onChanged: (String value) {
-            taskName = value;
-          },
-        ),
-        actions: <Widget>[
-          ElevatedButton(
-            child: const Text('Add'),
-            onPressed: () {
-              setState(() {
-                _todos.add(Todo(name: taskName));
-              });
-              Navigator.of(context).pop();
+      context: context,
+      builder: (BuildContext context) {
+        String? taskName;
+        return AlertDialog(
+          title: const Text('Add Todo'),
+          content: TextField(
+            autofocus: true,
+            onChanged: (String value) {
+              taskName = value;
             },
           ),
-          ElevatedButton(
-            child: const Text('Cancel'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-        },
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Add'),
+              onPressed: () {
+                setState(() {
+                  _todos.add(Todo(name: taskName));
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
