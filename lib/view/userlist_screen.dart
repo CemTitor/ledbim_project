257a63b4241f0af8077b48fb9f74c92d20f29629
CemYilmaz <cemyilmaz463@gmail.com>
2023-02-05 21:56:02 +0300

    added userlist_screen

---
+++ lib/view/userlist_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class UserListScreen extends StatefulWidget {
+  const UserListScreen({super.key});
+
+  @override
+  _UserListScreenState createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User List'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              // Navigator.pushReplacement(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) => LoginScreen(),
+              //   ),
+              // );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () async {
+
+            },
+          ),
+        ],
+      ),
+      body: const Text(''),
+    );
+  }
+}
